d8e1aa03ab4898b2d703139049bdc276fcf8137e
sharif <sharif.jatri@gmail.com>
2022-08-04 13:13:28 +0600

    used reactive

--- src/use/useCounter.js
+++ src/use/useCounter.js
@@ -1,21 +1,29 @@
-import { ref, computed, watch } from "vue";
+import {computed, toRefs, reactive} from "vue";
 
 export function useCounter() {
-    const counter = ref(0)
-    const doubleCounter = computed(() => {
-        return counter.value * 2;
+    // const counter = ref(0)
+    // const doubleCounter = computed(() => {
+    //     return counter.value * 2;
+    // })
+
+    const data = reactive({
+        counter: 0,
+        doubleCounter: computed(() => data.counter * 2)
     })
+
     const addOne = () => {
-        counter.value += 1;
+        // counter.value += 1;
+        data.counter++;
     }
 
-    watch(counter, (newVal, oldVal) => {
-        console.log(counter, newVal, oldVal)
-    })
+    // watch(counter, (newVal, oldVal) => {
+    //     console.log(counter, newVal, oldVal)
+    // })
 
     return {
-        counter,
-        doubleCounter,
-        addOne
+        // counter,
+        // doubleCounter,
+        addOne,
+        ...toRefs(data),
     }
 }
